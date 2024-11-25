c845d6c8ca06173b634fb314419b9d1ecfc21dbe
Ray24 <a23310309@live.ceti.mx>
2024-11-25 12:58:48 -0600

    PruebaClassPelota

---
+++ include/Pelota.hpp
@@ -0,0 +1,45 @@
+#include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
+
+class Pelota
+{
+private:
+    float radio;
+    float posx;
+    float posy;
+    b2Body *cuerpoBola;
+
+public:
+    Pelota(b2World &mundo, float radio, float posx, float posy)
+    {
+        this->radio = radio;
+        this->posx = posx;
+        this->posy = posy;
+
+        b2BodyDef cuerpoBolaDef;
+        cuerpoBolaDef.type = b2_dynamicBody;
+        cuerpoBolaDef.position.Set(this->posx, this->posy);
+        this->cuerpoBola = mundo.CreateBody(&cuerpoBolaDef);
+
+        b2CircleShape formaBola;
+        formaBola.m_radius = radio;
+
+        b2FixtureDef fixtureBolaDef;
+        fixtureBolaDef.shape = &formaBola;
+        fixtureBolaDef.density = 0.01f;
+        fixtureBolaDef.friction = 0.7f;
+        cuerpoBola->CreateFixture(&fixtureBolaDef);
+    }
+    ~Pelota() {}
+
+    sf::CircleShape ObtenerFiguraPe()
+    {
+        sf::CircleShape bola(this->radio);
+        bola.setOrigin(this->radio, this->radio);
+        bola.setFillColor(sf::Color::Red);
+        bola.setPosition(
+            cuerpoBola->GetPosition().x,
+            cuerpoBola->GetPosition().y);
+        return bola;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,58 @@
 #include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
+#include <iostream>
+#include <Plataforma.hpp>
+#include <Pelota.hpp>
+using namespace std;
+
+sf::RenderWindow ventana(sf::VideoMode(1366, 720), "Redball!");
 
 int main()
 {
 
-    sf::RenderWindow ventana(sf::VideoMode(1280, 720), "Redball!");
-   
+    int fuerza = 2;
+
+    b2Vec2 vectorGravedad(0.0f, 10.0f);
+    b2World mundo(vectorGravedad);
+
+    Plataforma p1(mundo, 200, 500, 10, 600);
+    Plataforma p2(mundo, 1000, 480, 10, 600);
+    Plataforma p3(mundo, 1000, 200, 10, 300);
+
+    Pelota pe1(mundo, 10, 400, 300);
 
     while (ventana.isOpen())
     {
-    
-        sf::Event event;
-        while (ventana.pollEvent(event))
+        sf::Event evento;
+        while (ventana.pollEvent(evento))
         {
-            if (event.type == event.Closed)
+            if (evento.type == sf::Event::Closed)
                 ventana.close();
         }
-    
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            pe1->ApplyForceToCenter(b2Vec2(-fuerza, 0.0f), true);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            pe1->ApplyForceToCenter(b2Vec2(fuerza, 0.0f), true);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            pe1->ApplyForceToCenter(b2Vec2(0.0f, -35), true);
+
+        // Calcular simulacion fisica
+        mundo.Step(1.0f / 60.0f, 6, 2);
+        // cout << "Posicion de la bola: " << cuerpoBola->GetPosition().x << ", " << cuerpoBola->GetPosition().y << endl;
+
+        ventana.clear();
+
+        ventana.draw(p1.ObtenerFigura());
+        ventana.draw(p2.ObtenerFigura());
+        ventana.draw(p3.ObtenerFigura());
+
+        ventana.draw(pe1.ObtenerFiguraPe());
+
+        ventana.display();
     }
+
+    return 0;
 }
